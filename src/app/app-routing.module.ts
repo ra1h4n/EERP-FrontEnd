d4f4759f879c67ec6cd33093c54c081dbe3d50af
Raihanuzzaman Rasel <ra1h4n@outlook.com>
2020-02-23 15:07:01 +0600

    Project Structure Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: '', redirectTo: '/dashboard', pathMatch: 'full'
+  },
+  {
+    path: 'product/add', component: AddProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideMenuComponent
+    SideMenuComponent,
+    DashboardComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
